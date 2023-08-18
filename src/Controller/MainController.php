886bfc1488457705a48ec29a7005090d02dc60f9
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-18 22:20:19 +0200

    add MainController

---
+++ src/Controller/MainController.php
@@ -0,0 +1,15 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController
+{
+    #[Route('/')]
+    public function test()
+    {
+        dd('salut !');
+    }
+}
