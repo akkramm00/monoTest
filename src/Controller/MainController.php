0ceaad993a3908b5817469a3d911a6bbc5ab551c
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-19 02:10:00 +0200

    add argument

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,15 +3,21 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController
+class MainController extends AbstractController
 {
-    #[Route('/test')]
-    public function test()
+    #[Route('/user/{name}')]
+    public function test($name)
     {
 
-        dd("Page principale");
+        $age = 19;
+
+        return $this->render('main.html.twig', [
+            'prenom' => $name,
+            'age' => $age
+        ]);
     }
 }
