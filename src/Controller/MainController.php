e1d85967638e58d5406590416efb4b39f48f90b3
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-19 03:02:51 +0200

    add all

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,9 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route(name: 'app_')]
 class MainController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: 'main')]
     public function test()
     {
         return $this->render('main.html.twig');
@@ -17,7 +18,7 @@ class MainController extends AbstractController
 
 
 
-    #[Route('/contact')]
+    #[Route('/Nous contactez', name: 'contact')]
     public function contact()
     {
         return $this->render('contact.html.twig');
